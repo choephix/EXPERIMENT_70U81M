8d17c4d1c6e95c90a234e2fc843c4705db7a462c
Stefan <choephix@gmail.com>
2023-07-28 21:44:29 +0000

    --

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,53 +1,74 @@
+import { useFrame } from '@react-three/fiber';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import React, { useEffect, useRef, useState } from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 type ModelProps = {
   url: string;
   camera: THREE.Camera;
 };
 
+const loader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+loader.setDRACOLoader(dracoLoader);
+
 const Model: React.FC<ModelProps> = ({ url, camera }: ModelProps) => {
   const [model, setModel] = useState<THREE.Object3D | null>(null);
   const ref = useRef<THREE.Object3D | null>(null);
 
-  // const { set, gl } = useThree();
-  // useEffect(() => {
-  //   // This will set the pixel ratio to 1/4th its original value
-  //   gl.setPixelRatio(window.devicePixelRatio * 0.5);
-  // }, [gl]);
-
-  // useEffect(() => {
-  //   new GLTFLoader().load(url, gltf => {
-  //     setModel(gltf.scene);
-  //   });
-  // }, [url]);
-
   useEffect(() => {
-    new GLTFLoader().load(url, gltf => {
+    loader.load(url, gltf => {
       setModel(gltf.scene);
+
       const s = new THREE.Box3().setFromObject(gltf.scene);
       const center = s.getCenter(new THREE.Vector3());
       const size = s.getSize(new THREE.Vector3());
 
       //Look at the center of the model
       camera.position.copy(center);
-      camera.position.x += size.length(); //Move camera to some distance
+      camera.position.x += .1 * size.length(); //Move camera to some distance
       camera.lookAt(center);
-
-      // Log dimensions of all objects
-      gltf.scene.traverse(child => {
-        if (child instanceof THREE.Mesh) {
-          const bbox = new THREE.Box3().setFromObject(child);
-          const size = bbox.getSize(new THREE.Vector3());
-          console.log([size.x, size.y, size.z]);
-          child.userData.size = [size.x, size.y, size.z];
-          child.userData.sizeMax = Math.max(size.x, size.y, size.z);
-        }
-      });
     });
   }, [url, camera]);
+
+  if (!model) {
+    return null;
+  }
+
+  return (
+    <>
+      <ModelOptimizer model={model} camera={camera} />
+      <primitive object={model} ref={ref} />
+    </>
+  );
+};
+
+type ModelOptimizerProps = {
+  model: THREE.Object3D;
+  camera: THREE.Camera;
+};
+
+const ModelOptimizer: React.FC<ModelOptimizerProps> = ({ model, camera }: ModelOptimizerProps) => {
+  const handleClick = (child: THREE.Mesh) => {
+    console.log('Clicked object:', child.userData);
+  };
+
+  useEffect(() => {
+    model.traverse(child => {
+      if (child instanceof THREE.Mesh) {
+        const bbox = new THREE.Box3().setFromObject(child);
+        const size = bbox.getSize(new THREE.Vector3());
+        child.userData.size = [size.x, size.y, size.z];
+        child.userData.sizeMax = Math.max(size.x, size.y, size.z);
+
+        console.log('sizeMax', child.userData.sizeMax);
+        
+        child.addEventListener('click', () => handleClick(child));
+      }
+    });
+  }, [model]);
 
   useFrame(() => {
     // This assumes that the model is an Object3D (or derived class) instance.
@@ -74,19 +95,29 @@ const Model: React.FC<ModelProps> = ({ url, camera }: ModelProps) => {
 
       // Only the first 100 closest should be visible
       distances.forEach((item, index) => {
-        // item.object.visible = index < 1000;
-        item.object.visible = true;
-
-        const sizeMax = item.object.userData.sizeMax as number;
-        const distanceFromCamera = item.distance as number;
-        // const ratio = distanceFromCamera / sizeMax;
-
-        item.object.visible = sizeMax > 0.5 || distanceFromCamera < 15; //
+        updateObjectVisibility(item, index);
       });
     }
   });
 
-  return model ? <primitive object={model} ref={ref} /> : null;
+  return null;
 };
+
+function updateObjectVisibility(
+  item: {
+    distance: number;
+    object: THREE.Mesh;
+  },
+  index: number
+) {
+  // item.object.visible = index < 1000;
+  item.object.visible = true;
+
+  const sizeMax = item.object.userData.sizeMax as number;
+  const distanceFromCamera = item.distance as number;
+  // const ratio = distanceFromCamera / sizeMax;
+
+  // item.object.visible = sizeMax > 0.5 || distanceFromCamera < 20; //
+}
 
 export default Model;
--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -9,6 +9,7 @@ const Viewer = () => {
   const [url, setUrl] = useState<string>('');
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const controlsRef = useRef<any>(null);
 
   const handleDrop = (file: File) => {
     setUrl(URL.createObjectURL(file));
@@ -27,6 +28,14 @@ const Viewer = () => {
     setIsDragging(false);
   };
 
+  const handleZoom = () => {
+    if (!controlsRef.current) return;
+    
+    const distance = controlsRef.current?.getDistance() || 0;
+    const zoomSpeed = distance > 10 ? 1 : distance / 200;
+    controlsRef.current.zoomSpeed = zoomSpeed;
+  };
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       {url.length === 0 ? (
@@ -36,16 +45,20 @@ const Viewer = () => {
           <div style={{ position: 'absolute', top: '15px', left: '15px', zIndex: 1 }}>
             <Button onClick={handleClear}>Clear Model</Button>
           </div>
+
           <Canvas
             style={{ width: '100%', height: '100%' }}
             dpr={isDragging ? 0.5 : 1}
             ref={canvasRef}
           >
             <OrbitControls //
-              // dampingFactor={0.01}
-              enableDamping={false}
+              ref={controlsRef}
+              dampingFactor={0.5}
+              enableZoom
+              panSpeed={0.15}
               onStart={handleStart}
               onEnd={handleEnd}
+              onChange={handleZoom}
             />
             <Scene url={url} />
           </Canvas>
