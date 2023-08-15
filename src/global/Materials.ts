712fd1cf082179041bc1826e8579f8db8f3bf7bf
Stefan <choephix@gmail.com>
2023-08-15 12:34:00 +0000

    Add shading

--- src/global/Materials.ts
+++ src/global/Materials.ts
@@ -7,25 +7,45 @@ export module Materials {
   });
 
   export const DISPLAY_MATERIAL = new ShaderMaterial({
-    uniforms: { selectedSourceMeshIndex: { value: [0, 0, 0] } },
+    uniforms: {
+      selectedSourceMeshIndex: { value: [0, 0, 0] },
+      lightColor: { value: [1, 1, 1] },
+      lightDirection: { value: [0.5, 1, 1] },
+    },
     vertexShader: `
     precision highp float;
 
-    uniform vec3 selectedSourceMeshIndex;
-    
     attribute vec3 color;
     attribute vec3 sourceMeshIndex;
 
+    uniform vec3 selectedSourceMeshIndex;
+    uniform vec3 lightColor;
+    uniform vec3 lightDirection;
+
     varying vec3 vColor;
-    
+
     bool vec3Equal(vec3 a, vec3 b, float tolerance) {
-      // return all(equal(a, b));
       return length(a - b) < tolerance;
     }
-    
+
     void main() {
+      // Calculate base color depending on whether the mesh is selected
       bool selected = vec3Equal(sourceMeshIndex, selectedSourceMeshIndex, .5);
-      vColor = selected ? vec3(0.129, 0.508, 0.822) : vec3(0.8);
+      vColor = selected ? vec3(0.129, 0.508, 0.822) : vec3(0.5);
+
+      // Diffuse Lambertian lighting
+      vec3 normLightDirection = normalize(lightDirection);
+      vec3 negNormLightDirection = -normLightDirection;
+      vec3 normal = normalize(normalMatrix * normal);
+
+      // Calculate the diffuse component for both lights
+      float diffLight1 = max(dot(normal, normLightDirection), 0.0);
+      float diffLight2 = max(dot(normal, negNormLightDirection), 0.0);
+
+      float diffLightsTotal = diffLight1 + diffLight2;
+      float diffLightsWithAmbient = 0.5 + diffLightsTotal;
+
+      vColor = vColor * diffLightsWithAmbient * lightColor;
 
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }`,
